be8a898a1441146eb837b7a396c842bac6ba8a0b
SUNGOD3 <72863539+SUNGOD3@users.noreply.github.com>
2021-03-14 10:31:21 +0800

    Revert "Revert "Feat user api""

--- src/backend/backend/urls.py
+++ src/backend/backend/urls.py
@@ -14,8 +14,28 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API",
+        default_version='v1',
+        description="Test",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="rexwu.1123@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('api/user/', include('user.urls')),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0))
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
